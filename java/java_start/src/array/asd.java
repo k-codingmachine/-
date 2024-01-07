f617fe4bbb7480fadb071e6f94d33f7ff7d1f012
k-codingmachine <aj8072@naver.com>
2024-01-07 22:24:59 +0900

    Java

---
+++ java/java_start/src/array/asd.java
@@ -0,0 +1,14 @@
+package array;
+
+public class asd {
+    public static void main(String[] args) {
+
+      int result = add(3, 5);
+        System.out.println(result);
+    }
+
+    public static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
