60628c8ed9184dd4579af05f71a4a0f74d2bfbae
k-codingmachine <aj8072@naver.com>
2024-01-09 21:27:12 +0900

    Javajungsik

--- javajungsuk/ch06/src/Ex6_4.java
+++ javajungsuk/ch06/src/Ex6_4.java
@@ -5,12 +5,15 @@ class Ex6_4 {
 		long result2 = mm.subtract(5L, 3L);
 		long result3 = mm.multiply(5L, 3L);
 		double result4 = mm.divide(5L, 3L);
+		long result5 = mm.max(3L, 5L);
 		mm.gugudan(3);
 
 		System.out.println("add(5L, 3L) = " + result1);
 		System.out.println("subtract(5L, 3L) = " + result2);
 		System.out.println("multiply(5L, 3L) = " + result3);
 		System.out.println("divide(5L, 3L) = " + result4);
+		System.out.println("max(3L, 5L) = " + result5);
+
 	}
  }
 
@@ -32,6 +35,11 @@ class Ex6_4 {
 		return result;
 	//	return a + b;	
 	}
+	// 두 값을 받아서 둘중에 큰 값을 반환하는 메소드를 작성하시오.
+	long max (long a, long b) {
+		return a>b ? a : b;
+	}
+	
 	long subtract(long a, long b) { return a - b; }
 	long multiply(long a, long b) { return a * b; }
 	double divide(double a, double b) {
