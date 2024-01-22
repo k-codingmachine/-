056d3edb3d33b7840648ed585596540840763bc7
k-codingmachine <aj8072@naver.com>
2024-01-22 21:57:55 +0900

    python

---
+++ python/hello.py
@@ -0,0 +1,17 @@
+a = "12345678"
+
+print(a[::2]) 
+
+number = 10
+day = "three"
+a = "I ate %s apples. so I was sick for %s days." % (1.2, 3)
+
+print(a)
+
+a = "hobby"
+
+print(a.find('b')) # b가 들어간것을 찾는다.(첫번째 값)
+
+a = "Life is too short"
+
+print(a.split()) # 문자열 자료형이 있으면 띄어쓰기 기준으로 자르는 함수
---
+++ python/hello2.py
@@ -0,0 +1,18 @@
+
+a = 3
+b = 4
+print(a * b)
+print(a / b)
+print(a + b)
+print(a - b)
+print(a // b) #몫
+
+a = "Hello World"
+a = "Life is too short \nYou need python"
+b = 'hello'
+c = 'world'
+print(a)
+print(b+c)
+
+a = "12345678"
+print(a[8:])
