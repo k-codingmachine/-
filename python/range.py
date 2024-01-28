ff6d604bf93d2caa00842d46d4ff2b0e7a18ef20
k-codingmachine <aj8072@naver.com>
2024-01-28 23:17:24 +0900

    python

---
+++ python/range.py
@@ -0,0 +1,4 @@
+for i in range(2, 10): # range는 2~9까지의 숫자
+    for j in range(1,10): # 1~9
+        print(i*j, end=" ")
+    print('')
---
+++ python/전형적인 for문.py
@@ -0,0 +1,10 @@
+test_list = ['one', 'two', 'three']
+for i in test_list: # i에 리스트를 하나씩 담는다.
+    print(i)
+
+
+a = [(1,2), (3,4), (5,6)]
+for (first, last) in a:
+    print(first)
+    print(last)
+    
