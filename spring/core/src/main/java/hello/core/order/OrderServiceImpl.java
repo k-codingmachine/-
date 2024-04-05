2943e07547142f1761a2788167ec6149c3601245
k-codingmachine <aj8072@naver.com>
2024-04-05 23:31:20 +0900

    spring

--- spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-//@primary
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
