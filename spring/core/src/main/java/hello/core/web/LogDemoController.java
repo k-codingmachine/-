e0e6b8b0c675b6d2539236e6b08a8bc429ae22d6
k-codingmachine <aj8072@naver.com>
2024-04-12 20:21:23 +0900

    spring

--- spring/core/src/main/java/hello/core/web/LogDemoController.java
+++ spring/core/src/main/java/hello/core/web/LogDemoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// 서버 미스
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
